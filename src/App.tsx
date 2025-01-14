c97db1c333713dea1ed9ab8480789baf6f20a6f7
Jszigeti <j.szigeti1993@gmail.com>
2025-01-14 16:17:07 +0100

    refactor App component layout by removing header and simplifying structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import ChatTab from "./views/ChatTab";
 
 function App() {
   return (
-    <div className="relative min-h-screen bg-gray-50">
-      <h1 className="text-3xl font-bold text-center pt-10">
-        Bienvenue sur mon portfolio
-      </h1>
-      {/* Chat Tab */}
+    <div className="min-h-screen">
       <ChatTab />
     </div>
   );
